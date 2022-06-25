5415ecb672b890691a662d52779c5e4b29896f9a
Mubeen31 <qs6272527@gmail.com>
2022-06-25 11:58:21 +0100

    first commit

--- forcasted_data.py
+++ forcasted_data.py
@@ -242,46 +242,46 @@ with open("hourly_weather_forecasted_data.csv", "a", newline = '\n') as f:
          wind_speed_0, wind_direction_0, humidity_0, visibility_0, uvi_index_0, uvi_index_text_0, pre_proba_0,
          rain_proba_0, cloud_cover_0])
     writer.writerow(
-        [solar_irradiance_1, icon_phrase_1, temperature_1, real_feel_temperature_1, dew_point_1, wind_speed_1,
+        [dte_1, tme_1, solar_irradiance_1, icon_phrase_1, temperature_1, real_feel_temperature_1, dew_point_1, wind_speed_1,
          wind_direction_1, humidity_1, visibility_1, uvi_index_1, uvi_index_text_1, pre_proba_1, rain_proba_1,
          cloud_cover_1])
     writer.writerow(
-        [solar_irradiance_2, icon_phrase_2, temperature_2, real_feel_temperature_2, dew_point_2, wind_speed_2,
+        [dte_2, tme_2, solar_irradiance_2, icon_phrase_2, temperature_2, real_feel_temperature_2, dew_point_2, wind_speed_2,
          wind_direction_2, humidity_2, visibility_2, uvi_index_2, uvi_index_text_2, pre_proba_2, rain_proba_2,
          cloud_cover_2])
     writer.writerow(
-        [solar_irradiance_3, icon_phrase_3, temperature_3, real_feel_temperature_3, dew_point_3, wind_speed_3,
+        [dte_3, tme_3, solar_irradiance_3, icon_phrase_3, temperature_3, real_feel_temperature_3, dew_point_3, wind_speed_3,
          wind_direction_3, humidity_3, visibility_3, uvi_index_3, uvi_index_text_3, pre_proba_3, rain_proba_3,
          cloud_cover_3])
     writer.writerow(
-        [solar_irradiance_4, icon_phrase_4, temperature_4, real_feel_temperature_4, dew_point_4, wind_speed_4,
+        [dte_4, tme_4, solar_irradiance_4, icon_phrase_4, temperature_4, real_feel_temperature_4, dew_point_4, wind_speed_4,
          wind_direction_4, humidity_4, visibility_4, uvi_index_4, uvi_index_text_4, pre_proba_4, rain_proba_4,
          cloud_cover_4])
     writer.writerow(
-        [solar_irradiance_5, icon_phrase_5, temperature_5, real_feel_temperature_5, dew_point_5, wind_speed_5,
+        [dte_5, tme_5, solar_irradiance_5, icon_phrase_5, temperature_5, real_feel_temperature_5, dew_point_5, wind_speed_5,
          wind_direction_5, humidity_5, visibility_5, uvi_index_5, uvi_index_text_5, pre_proba_5, rain_proba_5,
          cloud_cover_5])
     writer.writerow(
-        [solar_irradiance_6, icon_phrase_6, temperature_6, real_feel_temperature_6, dew_point_6, wind_speed_6,
+        [dte_6, tme_6, solar_irradiance_6, icon_phrase_6, temperature_6, real_feel_temperature_6, dew_point_6, wind_speed_6,
          wind_direction_6, humidity_6, visibility_6, uvi_index_6, uvi_index_text_6, pre_proba_6, rain_proba_6,
          cloud_cover_6])
     writer.writerow(
-        [solar_irradiance_7, icon_phrase_7, temperature_7, real_feel_temperature_7, dew_point_7, wind_speed_7,
+        [dte_7, tme_7, solar_irradiance_7, icon_phrase_7, temperature_7, real_feel_temperature_7, dew_point_7, wind_speed_7,
          wind_direction_7, humidity_7, visibility_7, uvi_index_7, uvi_index_text_7, pre_proba_7, rain_proba_7,
          cloud_cover_7])
     writer.writerow(
-        [solar_irradiance_8, icon_phrase_8, temperature_8, real_feel_temperature_8, dew_point_8, wind_speed_8,
+        [dte_8, tme_8, solar_irradiance_8, icon_phrase_8, temperature_8, real_feel_temperature_8, dew_point_8, wind_speed_8,
          wind_direction_8, humidity_8, visibility_8, uvi_index_8, uvi_index_text_8, pre_proba_8, rain_proba_8,
          cloud_cover_8])
     writer.writerow(
-        [solar_irradiance_9, icon_phrase_9, temperature_9, real_feel_temperature_9, dew_point_9, wind_speed_9,
+        [dte_9, tme_9, solar_irradiance_9, icon_phrase_9, temperature_9, real_feel_temperature_9, dew_point_9, wind_speed_9,
          wind_direction_9, humidity_9, visibility_9, uvi_index_9, uvi_index_text_9, pre_proba_9, rain_proba_9,
          cloud_cover_9])
     writer.writerow(
-        [solar_irradiance_10, icon_phrase_10, temperature_10, real_feel_temperature_10, dew_point_10, wind_speed_10,
+        [dte_10, tme_10, solar_irradiance_10, icon_phrase_10, temperature_10, real_feel_temperature_10, dew_point_10, wind_speed_10,
          wind_direction_10, humidity_10, visibility_10, uvi_index_10, uvi_index_text_10, pre_proba_10, rain_proba_10,
          cloud_cover_10])
     writer.writerow(
-        [solar_irradiance_11, icon_phrase_11, temperature_11, real_feel_temperature_11, dew_point_11, wind_speed_11,
+        [dte_11, tme_11, solar_irradiance_11, icon_phrase_11, temperature_11, real_feel_temperature_11, dew_point_11, wind_speed_11,
          wind_direction_11, humidity_11, visibility_11, uvi_index_11, uvi_index_text_11, pre_proba_11, rain_proba_11,
          cloud_cover_11])
