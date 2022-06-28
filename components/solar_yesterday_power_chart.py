b4400b92fc35b0f49f2ff3e58193b8e0ea24c643
Mubeen31 <qs6272527@gmail.com>
2022-06-28 15:56:10 +0100

    first commit

--- components/solar_yesterday_power_chart.py
+++ components/solar_yesterday_power_chart.py
@@ -7,7 +7,9 @@ from dash.exceptions import PreventUpdate
 import pandas as pd
 import numpy as np
 from datetime import datetime, date, time
+from datetime import timedelta
 from sklearn import linear_model
+from sklearn.ensemble import RandomForestRegressor
 import sqlalchemy
 from dash import dash_table as dt
 import time
@@ -26,6 +28,9 @@ html.Div([
 
 
 def solar_yesterday_power_chart_value(n_intervals):
+    n = 1
+    now = datetime.now() + timedelta(hours = n)
+    time_name = now.strftime('%H:%M:%S')
     header_list = ['Date Time', 'Voltage', 'Current']
     df = pd.read_csv('sensors_data.csv', names = header_list)
     df['Power (W)'] = df['Voltage'] * df['Current']
@@ -39,6 +44,9 @@ def solar_yesterday_power_chart_value(n_intervals):
     df['time'].iloc[-1].strftime('%H:%M')
     df['time'] = df['time'].astype(str)
     df['hour'] = df['hour'].astype(str)
+    df['Time'] = pd.to_datetime(df['Date Time']).dt.time
+    df['Hour'] = pd.to_datetime(df['Date Time']).dt.hour
+    df['Time'] = df['Time'].astype(str)
     today_date = df['Date'].unique()
 
     first_hour = df[(df['Date'] == today_date[-2]) & (df['time'] >= '00:00:00') & (df['time'] <= '00:59:59')]
@@ -123,17 +131,105 @@ def solar_yesterday_power_chart_value(n_intervals):
     hourly_data_and_hours = {'Hours': hours, 'Hourly Data': hourly_data}
     hourly_data_and_hours_df = pd.DataFrame(hourly_data_and_hours)
 
+    rearrange_columns = ['Date Time', 'Date', 'Time', 'Hour', 'Voltage', 'Current', 'Power (W)', 'Power (KW)']
+    df = df[rearrange_columns]
+    unique_date = df['Date'].unique()
+    filter_daily_values = df[(df['Date'] > '2022-06-24') & (df['Date'] <= unique_date[-2])][
+        ['Date', 'Hour', 'Power (KW)']]
+    daily_hourly_values = filter_daily_values.groupby(['Date', 'Hour'])['Power (KW)'].sum().reset_index()
+
+    header_list = ['Date', 'Time', 'SolarIrradiance (W/m2)', 'weather status', 'Temp (°C)', 'RealFeelTemp (°C)',
+                   'DewPoint (°C)',
+                   'Wind (km/h)',
+                   'Direction', 'Hum (%)', 'Visibility (km)', 'UVIndex', 'UVIndexText', 'PreProbability (%)',
+                   'RainProbability (%)',
+                   'CloudCover (%)']
+    weather_data = pd.read_csv('hourly_weather_forecasted_data.csv', names = header_list,
+                               encoding = 'unicode_escape')
+    weather_data.drop(
+        ['Date', 'Time', 'RealFeelTemp (°C)', 'DewPoint (°C)', 'Wind (km/h)', 'Direction', 'Visibility (km)', 'UVIndex',
+         'UVIndexText', 'PreProbability (%)', 'RainProbability (%)', 'weather status'], axis = 1,
+        inplace = True)
+
+    df1 = pd.concat([daily_hourly_values, weather_data], axis = 1)
+    df1.drop(['Date', 'Hour'], axis = 1, inplace = True)
+    df1.loc[df1['SolarIrradiance (W/m2)'] == 0, ['Temp (°C)', 'Hum (%)', 'CloudCover (%)']] = 0
+
+    filter_last_day_values = df[df['Date'] == unique_date[-2]][['Date', 'Hour', 'Power (KW)']]
+    last_day_hourly_values = filter_last_day_values.groupby(['Date', 'Hour'])['Power (KW)'].sum().reset_index()
+    last_day_hourly_values_sum = last_day_hourly_values['Power (KW)'].sum()
+
+    filter_yes_values = df[(df['Date'] >= '2022-06-25') & (df['Date'] <= unique_date[-3])][
+        ['Date', 'Hour', 'Power (KW)']]
+    yes_hourly_values = filter_yes_values.groupby(['Date', 'Hour'])['Power (KW)'].sum().reset_index()
+    header_list = ['Date', 'Time', 'SolarIrradiance (W/m2)', 'weather status', 'Temp (°C)', 'RealFeelTemp (°C)',
+                   'DewPoint (°C)', 'Wind (km/h)',
+                   'Direction', 'Hum (%)', 'Visibility (km)', 'UVIndex', 'UVIndexText', 'PreProbability (%)',
+                   'RainProbability (%)',
+                   'CloudCover (%)']
+    weather_data1 = pd.read_csv('hourly_weather_forecasted_data.csv', names = header_list, encoding = 'unicode_escape')
+    weather_unique_date = weather_data1['Date'].unique()
+    filter_weather_yes_values = weather_data1[
+        (weather_data1['Date'] >= '2022-06-25') &
+        (weather_data1['Date'] <= weather_unique_date[-3])][['SolarIrradiance (W/m2)',
+                                                             'Temp (°C)', 'Hum (%)',
+                                                             'CloudCover (%)']]
+    yes_df1 = pd.concat([yes_hourly_values, filter_weather_yes_values], axis = 1)
+    yes_df1.drop(['Date', 'Hour'], axis = 1, inplace = True)
+    yes_df1.loc[yes_df1['SolarIrradiance (W/m2)'] == 0, ['Temp (°C)', 'Hum (%)', 'CloudCover (%)']] = 0
+    yes_count_total_rows = len(yes_df1)
+    yes_independent_columns = yes_df1[['SolarIrradiance (W/m2)', 'Temp (°C)', 'Hum (%)', 'CloudCover (%)']][
+                              0:yes_count_total_rows]
+    yes_dependent_column = yes_df1['Power (KW)'][0:yes_count_total_rows]
+    yes_reg = linear_model.LinearRegression(fit_intercept = False)
+    yes_reg.fit(yes_independent_columns, yes_dependent_column)
+    forcasted_yes_values = weather_data1[(weather_data1['Date'] == weather_unique_date[-2])][['SolarIrradiance (W/m2)',
+                                                                                              'Temp (°C)', 'Hum (%)',
+                                                                                              'CloudCover (%)']]
+    forcasted_yes_values.loc[
+        forcasted_yes_values['SolarIrradiance (W/m2)'] == 0, ['Temp (°C)', 'Hum (%)', 'CloudCover (%)']] = 0
+    return_array = yes_reg.predict(forcasted_yes_values)
+    predicted_data = pd.DataFrame(return_array, columns = ['Power (KW)'])
+
+    rfr_yes = RandomForestRegressor(n_estimators = 100, random_state = 0)
+    rfr_yes.fit(yes_independent_columns, yes_dependent_column)
+    rfr_yes_return_array = rfr_yes.predict(forcasted_yes_values)
+    rfr_yes_predicted_data = pd.DataFrame(rfr_yes_return_array, columns = ['Power (KW)'])
+
     return {
         'data': [go.Scatter(
-            x = hourly_data_and_hours_df['Hours'],
-            y = hourly_data_and_hours_df['Hourly Data'],
-            mode = 'lines',
-            marker = dict(color = '#4DBFF1'),
-            hoverinfo = 'text',
-            hovertext =
-            '<b>Hour</b>: ' + hourly_data_and_hours_df['Hours'].astype(str) + '<br>' +
-            '<b>Solar Energy</b>: ' + [f'{x:,.5f} KWh' for x in hourly_data_and_hours_df['Hourly Data']] + '<br>'
-        )],
+                x = hourly_data_and_hours_df['Hours'],
+                y = predicted_data['Power (KW)'],
+                name = 'Yesterday Predicted Solar Energy (MVLR Model)',
+                mode = 'lines',
+                line = dict(color = 'firebrick', dash = 'dash'),
+                hoverinfo = 'text',
+                hovertext =
+                '<b>Hour</b>: ' + hourly_data_and_hours_df['Hours'].astype(str) + '<br>' +
+                '<b>Yesterday Predicted Solar Energy (MVLR Model)</b>: ' + [f'{x:,.5f} KWh' for x in predicted_data['Power (KW)']] + '<br>'
+            ),
+            go.Scatter(
+                x = hourly_data_and_hours_df['Hours'],
+                y = rfr_yes_predicted_data['Power (KW)'],
+                name = 'Yesterday Predicted Solar Energy (RFR Model)',
+                mode = 'lines',
+                line = dict(color = '#FF7F50', dash = 'dot'),
+                hoverinfo = 'text',
+                hovertext =
+                '<b>Hour</b>: ' + hourly_data_and_hours_df['Hours'].astype(str) + '<br>' +
+                '<b>Yesterday Predicted Solar Energy (RFR Model)</b>: ' + [f'{x:,.5f} KWh' for x in rfr_yes_predicted_data['Power (KW)']] + '<br>'
+            ),
+            go.Scatter(
+                x = hourly_data_and_hours_df['Hours'],
+                y = hourly_data_and_hours_df['Hourly Data'],
+                name = 'Yesterday Solar Energy',
+                mode = 'lines',
+                marker = dict(color = '#4DBFF1'),
+                hoverinfo = 'text',
+                hovertext =
+                '<b>Hour</b>: ' + hourly_data_and_hours_df['Hours'].astype(str) + '<br>' +
+                '<b>Yesterday Solar Energy</b>: ' + [f'{x:,.5f} KWh' for x in hourly_data_and_hours_df['Hourly Data']] + '<br>'
+            )],
 
         'layout': go.Layout(
             plot_bgcolor = 'rgba(255, 255, 255, 0)',
@@ -181,10 +277,17 @@ def solar_yesterday_power_chart_value(n_intervals):
                     color = '#1a1a1a')
 
             ),
+            legend = {
+                'orientation': 'h',
+                'bgcolor': 'rgba(255, 255, 255, 0)',
+                'x': 0.5,
+                'y': 1.2,
+                'xanchor': 'center',
+                'yanchor': 'top'},
             font = dict(
                 family = "sans-serif",
                 size = 12,
-                color = 'black')
+                color = 'black'),
 
         )
 
