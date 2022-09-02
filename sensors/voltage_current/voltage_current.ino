8340126710b6b0643351c47079a43b466d7992e2
Mubeen Ali <qs6272527@gmail.com>
2022-09-02 09:53:45 +0100

    first commit

--- sensors/voltage_current/voltage_current.ino
+++ sensors/voltage_current/voltage_current.ino
@@ -46,7 +46,7 @@ void loop(){
 ///////solar current//////
   // Vout is read 1000 Times for precision
   for(int i = 0; i < 1000; i++) {
-    Vout = (Vout + (resADC * analogRead(A3)));   
+    Vout = (Vout + (resADC * analogRead(A2)));   
 //    delay(1);
   }
   
