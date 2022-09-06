4e69ea94f1eb4196a668ffafc28d740049018685
Mubeen31 <qs6272527@gmail.com>
2022-09-06 15:09:17 +0100

    first commit

--- components/energy_forcasting_card.py
+++ components/energy_forcasting_card.py
@@ -73,27 +73,40 @@ def random_forest_regression_card_value(n_intervals, select_trees, select_random
         rearrange_columns = ['Date Time', 'Date', 'Time', 'Hour', 'Voltage', 'Current', 'Power (W)', 'Power (KW)']
         df = df[rearrange_columns]
         unique_date = df['Date'].unique()
-        filter_daily_values = df[(df['Date'] > '2022-06-24') & (df['Date'] <= unique_date[-2])][
+        filter_daily_values = df[(df['Date'] > '2022-08-11') & (df['Date'] <= unique_date[-2])][
             ['Date', 'Hour', 'Power (KW)']]
         daily_hourly_values = filter_daily_values.groupby(['Date', 'Hour'])['Power (KW)'].sum().reset_index()
 
         header_list = ['Date', 'Time', 'SolarIrradiance (W/m2)', 'weather status', 'Temp (°C)', 'RealFeelTemp (°C)',
-                       'DewPoint (°C)',
-                       'Wind (km/h)',
+                       'DewPoint (°C)', 'Wind (km/h)',
                        'Direction', 'Hum (%)', 'Visibility (km)', 'UVIndex', 'UVIndexText', 'PreProbability (%)',
                        'RainProbability (%)',
                        'CloudCover (%)']
-        weather_data = pd.read_csv('https://raw.githubusercontent.com/Mubeen31/solar-power-and-weather-data/main/hourly_weather_forecasted_data.csv', names = header_list,
+        weather_data = pd.read_csv('hourly_weather_forecasted_data.csv', names = header_list,
                                    encoding = 'unicode_escape')
         weather_data['UV Index Text'] = pd.factorize(weather_data['UVIndexText'])[0]
-        weather_data.drop(['Date', 'Time', 'Direction', 'Visibility (km)',
-                           'UVIndexText', 'PreProbability (%)', 'RainProbability (%)', 'weather status',
-                           'CloudCover (%)', 'Hum (%)', 'DewPoint (°C)'], axis = 1,
-                          inplace = True)
+        weather_data.loc[
+            weather_data['SolarIrradiance (W/m2)'] == 0, ['weather status', 'Temp (°C)', 'RealFeelTemp (°C)',
+                                                          'DewPoint (°C)', 'Wind (km/h)',
+                                                          'Direction', 'Hum (%)', 'Visibility (km)', 'UVIndex',
+                                                          'UVIndexText', 'PreProbability (%)', 'RainProbability (%)',
+                                                          'CloudCover (%)', 'UV Index Text']] = 0
+        unique_weather_date = weather_data['Date'].unique()
+        hourly_weather = \
+        weather_data[(weather_data['Date'] >= '2022-08-11') & (weather_data['Date'] <= unique_weather_date[-2])][
+            ['Date', 'Time', 'SolarIrradiance (W/m2)', 'weather status', 'Temp (°C)', 'RealFeelTemp (°C)',
+             'DewPoint (°C)', 'Wind (km/h)',
+             'Direction', 'Hum (%)', 'Visibility (km)', 'UVIndex', 'UVIndexText', 'PreProbability (%)',
+             'RainProbability (%)',
+             'CloudCover (%)', 'UV Index Text']].reset_index()
+        hourly_weather.drop(['index', 'Date', 'Time', 'Direction', 'Visibility (km)',
+                             'UVIndexText', 'PreProbability (%)', 'RainProbability (%)', 'weather status',
+                             'CloudCover (%)', 'Hum (%)', 'DewPoint (°C)'], axis = 1, inplace = True)
 
-        df1 = pd.concat([daily_hourly_values, weather_data], axis = 1)
+        df1 = pd.concat([daily_hourly_values, hourly_weather], axis = 1)
         df1.drop(['Date', 'Hour'], axis = 1, inplace = True)
-        df1.loc[df1['SolarIrradiance (W/m2)'] == 0, ['Temp (°C)', 'RealFeelTemp (°C)', 'Wind (km/h)', 'UVIndex', 'UV Index Text']] = 0
+        df1.loc[df1['SolarIrradiance (W/m2)'] == 0, ['Temp (°C)', 'RealFeelTemp (°C)', 'Wind (km/h)', 'UVIndex',
+                                                     'UV Index Text']] = 0
 
     if time_name >= '00:00:00' and time_name <= '11:59:59':
         count_total_rows = len(df1) - 12
@@ -104,7 +117,7 @@ def random_forest_regression_card_value(n_intervals, select_trees, select_random
         rfr = RandomForestRegressor(n_estimators = select_trees, random_state = select_random_state)
         rfr.fit(independent_columns, dependent_column)
 
-        forcasted_data = df1[['SolarIrradiance (W/m2)', 'Temp (°C)', 'RealFeelTemp (°C)', 'Wind (km/h)', 'UVIndex', 'UV Index Text']].tail(12)
+        forcasted_data = weather_data[['SolarIrradiance (W/m2)', 'Temp (°C)', 'RealFeelTemp (°C)', 'Wind (km/h)', 'UVIndex', 'UV Index Text']].tail(12)
 
         return_array = list(rfr.predict(forcasted_data))
 
@@ -129,7 +142,7 @@ def random_forest_regression_card_value(n_intervals, select_trees, select_random
         rfr = RandomForestRegressor(n_estimators = select_trees, random_state = select_random_state)
         rfr.fit(independent_columns, dependent_column)
 
-        forcasted_data = df1[['SolarIrradiance (W/m2)', 'Temp (°C)', 'RealFeelTemp (°C)', 'Wind (km/h)', 'UVIndex', 'UV Index Text']].tail(24)
+        forcasted_data = weather_data[['SolarIrradiance (W/m2)', 'Temp (°C)', 'RealFeelTemp (°C)', 'Wind (km/h)', 'UVIndex', 'UV Index Text']].tail(24)
 
         return_array = list(rfr.predict(forcasted_data))
 
